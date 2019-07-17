cf2f4cea807cbe7b04ef87858782e4c698bad2d9
Spinob93 <gb.giuseppe93@gmail.com>
2019-07-17 11:48:08 +0200

    notifyMe

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -8,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
 
   constructor() { }
+
+  @Input() product;
+  @Output() notify = new EventEmitter();
 
   ngOnInit() {
   }
